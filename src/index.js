7719b6aca9856872dd48f766d10fec261db65e41
Abhinav Upadhyay <abhinavupadhyay822@gmail.com>
2024-10-14 14:31:04 +0530

    database setup done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
+import dotenv from 'dotenv'
 import express from 'express'
+import connectDB from './db/index.js'
+
+dotenv.config({
+  path: './env'
+})
+connectDB()
 const app = express()
 const port = process.env.PORT || 4000
 
